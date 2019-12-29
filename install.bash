2a31945a5969ec15c68093b8cd25b016211ef1fe
Pengji Zhang <me@pengjiz.com>
2019-12-29 12:51:16 -0500

    Synchronize all submodules

--- install.bash
+++ install.bash
@@ -8,6 +8,6 @@ dotbot_dir="dotbot"
 dotbot_bin="$base_dir/$dotbot_dir/bin/dotbot"
 
 cd "$base_dir"
-git -C "$dotbot_dir" submodule sync --quiet --recursive
+git submodule sync --quiet --recursive
 git submodule update --init --recursive
 "$dotbot_bin" -d "$base_dir" -c "$config" "$@"
