62dc1c7951aecf5e16533d0615c156048beaf714
Pengji Zhang <me@pengjiz.com>
2020-02-08 19:09:26 -0500

    Unify code styles

--- install.bash
+++ install.bash
@@ -2,9 +2,9 @@
 
 set -e
 
-config="install.conf.yaml"
+config='install.conf.yaml'
 base_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-dotbot_dir="dotbot"
+dotbot_dir='dotbot'
 dotbot_bin="$base_dir/$dotbot_dir/bin/dotbot"
 
 cd "$base_dir"
