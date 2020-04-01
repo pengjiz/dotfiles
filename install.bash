96d4b897be0864bceb82e02b76b1620fd7b95d2a
Pengji Zhang <me@pengjiz.com>
2020-04-01 17:59:06 -0400

    Improve comments

--- install.bash
+++ install.bash
@@ -8,6 +8,9 @@ dotbot_dir='dotbot'
 dotbot_bin="$base_dir/$dotbot_dir/bin/dotbot"
 
 cd "$base_dir"
+# NOTE: Synchronize and update all submodules. The provided script only does
+# this for Dotbot. However, here we have more submodules, and it seems better to
+# do it all at once.
 git submodule sync --quiet --recursive
 git submodule update --init --recursive
 "$dotbot_bin" -d "$base_dir" -c "$config" "$@"
