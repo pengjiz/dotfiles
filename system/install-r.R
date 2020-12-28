010fc5cfec902b27c1a30f4aed17817af8cd9827
Pengji Zhang <me@pengjiz.com>
2020-12-27 19:00:24 -0500

    Improve code styles

--- system/install-r.R
+++ system/install-r.R
@@ -1,32 +1,30 @@
 #!/usr/bin/env Rscript
 
 # Set package repository
-local({
-  r <- getOption("repos")
-  if(r["CRAN"] == "@CRAN@") {
-    r["CRAN"] <- "https://cloud.r-project.org/"
-    options(repos = r)
-  }
-})
+repos <- getOption("repos")
+if (repos["CRAN"] == "@CRAN@") {
+  repos["CRAN"] <- "https://cloud.r-project.org/"
+  options(repos = repos)
+}
 
 # Set user library
 #
 # NOTE: R_LIBS_USER is similar to PATH and is a colon separated string of paths.
 # Here we only consider the first one which is similar to builtin functions.
-user_libpath = path.expand(
+user_libpath <- path.expand(
   strsplit(Sys.getenv("R_LIBS_USER", unset = "~/.local/lib/R/library"),
            ":")[[1]])
-if(!dir.exists(user_libpath)) {
+if (!dir.exists(user_libpath)) {
   dir.create(user_libpath, recursive = TRUE)
 }
 .libPaths(user_libpath)
 
 # Install packages
-packages = c("lintr",
-             "devtools",
-             "rmarkdown",
-             "tidyverse",
-             "sf",
-             "lwgeom",
-             "RSQLite")
+packages <- c("lintr",
+              "devtools",
+              "rmarkdown",
+              "tidyverse",
+              "sf",
+              "lwgeom",
+              "RSQLite")
 install.packages(packages, lib = user_libpath)
