d6ee7064cc020eb39915d7b0be5cfc186f527be5
Pengji Zhang <me@pengjiz.com>
2022-08-28 19:05:37 -0400

    Make Bash scripts stricter

--- install.bash
+++ install.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -euo pipefail
 
 config='install.conf.yaml'
 base_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
--- system/install.bash
+++ system/install.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -euo pipefail
 
 function log {
   local color=''
